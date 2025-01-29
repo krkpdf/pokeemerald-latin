a7d63eb61811396ae9236a7010ce876052cf8f25
krk <pedro.salgado@tuta.io>
2025-01-29 13:56:19 +0100

    finish lavaridge

--- insert_text.py
+++ insert_text.py
@@ -6,7 +6,6 @@ import re
 
 progress = {}
 
-j = 0
 
 script_dir = os.path.dirname(__file__)
 
@@ -14,6 +13,11 @@ folder_path_a = os.path.join("..", "pokeemerald-master-translated") + os.sep # o
 folder_path_b = os.path.join(".", "text") + os.sep                           # input folder
 
 #print(folder_path_a, folder_path_b)
+
+def extract_literal(line):
+    number = line.find(":")
+    return (int(line[:number]), line[number+2:])
+
 
 def extract_quoted_text(line):
     # Find the indices of the first and second quotation marks
@@ -25,7 +29,6 @@ def extract_quoted_text(line):
         return (int(line[:number]), line[first_quote_index + 1:second_quote_index])
     else:
         return (-1, "")  # Return empty string if no quoted text found
-
     
 def replace_quoted_text(line, text):
     first_quote_index = line.find('"')
@@ -60,33 +63,43 @@ try :
             empty = " "
             print(f"================= {(myfile_path + ' '):=<70}")
 
-            if "ipynb" not in myfile_path:
+            if ".json" in myfile_path:
                 with open(file_path_base, 'r', encoding='utf-8') as input_file:
                     content = [line for line in input_file.readlines()]
 
                 with open(file_path, 'r', encoding='utf-8') as input_file:
-                    new_content = [extract_quoted_text(line) for line in input_file.readlines()]
-                    new_content = [c for c in new_content if c[0] != -1]
+                    new_content = [extract_literal(line) for line in input_file.readlines()]
 
-                    j+=1
                     for i, t in new_content:
                         n = i-1
-                        if j < 0:
-                            print(i, "a: " + content[n].replace("\n", ""), "b: " + t.replace("\n", ""))
-                            print(replace_quoted_text(content[n], t))
-                        content[n] = replace_quoted_text(content[n], t)
-
-
-                    #print("\n\n")
-
-                    #if i < 30:
-                    #    print(f"================= {myfile_path} =================")
-                    #    print("\n".join([f"{c[0]}: {c[1]}" for c in content]),"\n\n")
+                        content[n] = t
 
 
                 with open(file_path_base, 'w', encoding='utf-8') as output_file:
-                    #if "Littleroot" in myfile_path:
-                    #    print("".join(content))
+                    output_file.write("".join(content))
+
+
+            elif "ipynb" not in myfile_path:
+                with open(file_path_base, 'r', encoding='utf-8') as input_file:
+                    content = [line for line in input_file.readlines()]
+
+                with open(file_path, 'r', encoding='utf-8') as input_file:
+                    if ".json" in myfile_path:
+                        new_content = [extract_literal(line) for line in input_file.readlines()]
+
+                        for i, t in new_content:
+                            n = i-1
+                            content[n] = t 
+                            
+                    else: 
+                        new_content = [extract_quoted_text(line) for line in input_file.readlines()]
+                        new_content = [c for c in new_content if c[0] != -1]
+
+                        for i, t in new_content:
+                            n = i-1
+                            content[n] = replace_quoted_text(content[n], t)
+
+                with open(file_path_base, 'w', encoding='utf-8') as output_file:
                     output_file.write("".join(content))
     
 except Exception as e:
